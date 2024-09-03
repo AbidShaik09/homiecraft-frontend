f376ba36f07d8c625f4b47288e41ce04af9d4958
sabidhussain <sabidhussain@actalentservices.com>
2024-09-03 10:36:21 +0000

    MUI iconsMaterials

--- homie-craft/src/App.js
+++ homie-craft/src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
+    <>
+      
+    <Navbar/>
    <h3>Homie Craft</h3>
+    </>
   );
 }
 
