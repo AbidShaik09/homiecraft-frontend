ab80deafe85077d52105dafcc219c8e8c37b7a25
AbidShaik09 <102479021+AbidShaik09@users.noreply.github.com>
2024-09-11 00:59:20 +0530

    Added JSON-Server Compatibility to home page using CategoryRepository

--- homie-craft/src/pages/customer/Categories.jsx
+++ homie-craft/src/pages/customer/Categories.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { getCategories, getCategory } from '../../repository/categoryRepository/categoryRepository'
+import { Typography } from '@mui/material'
+import ItemHolder from '../../components/itemHolder/ItemHolder'
 
 function Categories() {
     let {id} = useParams()
+    useEffect(()=>{
+
+    },[])
+    let [items,setItems]= useState([])
+    useEffect(()=>{
+      
+    getCategory(id).then(res=>setItems(res))
+    },[])
   return (
-    <div>
-      <h1>Category : {id}</h1>
-    </div>
+   
+<>
+<Typography gutterBottom variant="h3" component="div"
+    sx={{ overflow: 'hidden', padding:'30px'}}>
+    Category : {id}
+   </Typography>
+   <ItemHolder items={items}/>
+
+</>
   )
 }
 
