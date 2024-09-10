db2b4ba035386734df9511f83fdd1b7f0db37400
pulkit <aajay@actalentservices.com>
2024-09-10 07:35:46 +0000

    Banner and Images Slider

--- homie-craft/src/components/banner/Banner.jsx
+++ homie-craft/src/components/banner/Banner.jsx
@@ -1,22 +1,34 @@
 import React from 'react'
 import './Banner.css' 
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick-theme.css'
+import 'slick-carousel/slick/slick.css'
 const Banner = (params) => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 3000,
+    autoplaySpeed: 5000,
+    cssEase: "linear",
+    nextArrow:false,
+    prevArrow:false
+  };
   return (
-    <div class="ms-4" className='banner-container'>
-        <div className='wrapper'>
-            <div className='wrapper-holder'>
-              <div ><img src={params.image} alt="" /></div>
-              <div ><img src={params.image} alt="" /></div>
-              <div ><img src={params.image} alt="" /></div>
-              <div ><img src={params.image} alt="" /></div>
+    <div class="w-80 h-10">
+        <Slider {...settings}>
+            <div>
+              <img src={params.image} alt="" />
             </div>
-        </div>
-        <div className='button-holder'>
-          <a href="#slider-img-1" className='button'></a>
-          <a href="#slider-img-2" className='button'></a>
-          <a href="#slider-img-3" className='button'></a>
-          <a href="#slider-img-4" className='button'></a>
-        </div>
+            <div>
+              <img src={params.image} alt="" />
+            </div>
+            <div>
+              <img src={params.image} alt="" />
+            </div>
+        </Slider>
     </div>
   )
 }
--- homie-craft/src/components/gallerySection/ItemGallery.jsx
+++ homie-craft/src/components/gallerySection/ItemGallery.jsx
@@ -1,24 +1,53 @@
 import React from 'react'
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick-theme.css'
+import 'slick-carousel/slick/slick.css'
 import './ItemGallery.css'
-const ItemGallery = () => {
+
+function NextArrow(props) {
+  const { className, style, onClick } = props;
   return (
-    <div>
-        <div className='gallery-wrap' >
-            <img src="https://th.bing.com/th/id/OIP.GG47jUbA5fuPPOM4hx5nuwHaJI?pid=ImgDet&w=178&h=219&c=7" id='backbtn' />
-            <div className="gallery">
-                <div>
-                    <span><img src="https://th.bing.com/th/id/OIP.0oxXnJ17HjSmlMkYE6I9-QHaEo?rs=1&pid=ImgDetMain" alt="" /></span>
-                    <span><img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/04/30/12/sustainable-fashion-brands-women-indybest-0.jpg" alt="" /></span>
-                    <span><img src="https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg" alt="" /></span>
-                </div>
-                <div>
-                    <span><img src="https://th.bing.com/th/id/OIP.0oxXnJ17HjSmlMkYE6I9-QHaEo?rs=1&pid=ImgDetMain" alt="" /></span>
-                    <span><img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/04/30/12/sustainable-fashion-brands-women-indybest-0.jpg" alt="" /></span>
-                    <span><img src="https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg" alt="" /></span>
-                </div>
-            </div>
-            <img src="https://www.pngfind.com/pngs/m/691-6915975_slide-right-white-big-prev-and-next-arrow.png" id='nextbtn' />
-        </div>
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "gray" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
+const ItemGallery = () => {
+  
+    var settings={
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      nextArrow: <NextArrow />,
+      prevArrow: <PrevArrow />
+    }
+  return (
+    <div className="image-container">
+        <Slider {...settings}>
+       <div className="images">
+        <img classNameme="images" src="https://th.bing.com/th/id/OIP.0oxXnJ17HjSmlMkYE6I9-QHaEo?rs=1&pid=ImgDetMain"  alt="" />
+       </div>
+       <div>
+        <img classNameme="images" src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/04/30/12/sustainable-fashion-brands-women-indybest-0.jpg"  alt="" />
+       </div>
+       <div >
+        <img classNameme="images" src="https://static.remove.bg/sample-gallery/graphics/bird-thumbnail.jpg"  alt="" />
+       </div>
+       </Slider>
     </div>
   )
 }
