b714ab71d2169ba8899e68ab21183c99bb10ce49
pulkit <aajay@actalentservices.com>
2024-09-17 03:53:56 +0000

    Implemented Item Page

--- homie-craft/src/pages/customer/Item.jsx
+++ homie-craft/src/pages/customer/Item.jsx
@@ -3,23 +3,66 @@ import { useNavigate, useParams } from 'react-router-dom'
 import ButtonSecondary from '../../components/button/secondary/ButtonSecondary'
 import ButtonPrimary from '../../components/button/primary/ButtonPrimary'
 import ItemGallery from '../../components/gallerySection/ItemGallery'
+import Button from '@mui/material/Button';
 import axios from 'axios'
 import { Fab, TextField } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Add';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 function Item() {
     let {id} = useParams()
     const [craft,setCraft] = useState({})
+    const [price,setPrice] = useState()
+    const [crafter,setCrafter] = useState({})
+    const [open, setOpen] = useState(false)
+    var purchaseMode=''
+    const handleClickOpenHome = () => {
+      setOpen(true);
+      setPurchaseMode('Home Delivery')
+    };
+    const handleClickOpenPick = () => {
+      setOpen(true);
+      setPurchaseMode('Pick from craffter')
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
     useEffect(()=>{
       axios.get(`http://localhost:5265/crafts/${id}`).then((res)=>{
+
         setCraft(res.data)
+        setPrice(res.data[0].price)
+        axios.get(`http://localhost:5265/crafter/${res.data[0].crafterId}`).then((res)=>{
+          setCrafter(res.data)
+
+        })
       })
     },[])
     const [quantity,setQuantity] = useState(0)
+    const [userMessage,setUserMessage] = useState()
+    const [puschaseMode,setPurchaseMode] = useState()
     const quantityHandler=(newQuanity)=>{
       if(newQuanity>=0 && newQuanity<=craft[0].quantity)
         setQuantity(newQuanity)
+    }
+
+    const orderRequestHandler=()=>{
+        axios.post(`http://localhost:5625/orderrequest`,{
+          craftId:craft[0].Id,
+          userId:localStorage.getItem('customer'),
+          user:localStorage.getItem('token'),
+          crafterId:craft[0].crafterId,
+          purchaseMode:purchaseMode,
+          quantity:quantity,
+          price:craft[0].price * quantity,
+          userMessage:userMessage
+        }).then((res)=>{alert('Order Request Successful')})
     }
   return (craft.length>0?
     <div>
@@ -27,37 +70,72 @@ function Item() {
         <div style={{width:"50vw"}}><ItemGallery images={craft[0].images}/></div>
         <div class="mt-4">
           <h3>Delivery Options</h3>
-          <div class="d-flex mt-5 gap-5">
-            <ButtonPrimary name='Home Delivery'/>
-            <ButtonPrimary name='Pick from Crafter'/>
+          <div class="d-flex mt-3 gap-5">
+            <ButtonPrimary name='Home Delivery' action={handleClickOpenHome}/>
+            <ButtonPrimary name='Pick from Crafter' action={handleClickOpenPick}/>
           </div>
         </div>
       </div>
-      <div class="mt-5 ms-5">
-        <h2>{craft[0].name}</h2>
-        <div class="d-flex gap-5">
-          <h4>{'₹ '+craft[0].price}</h4>
-          <ButtonSecondary name='Wishlist Item'  />
+      <div class="mt-5 ms-5 d-flex" >
+        <div>
+          <h2>{craft[0].name}</h2>
+          <div class="d-flex gap-5">
+            <h4>{'₹ '+craft[0].price}</h4>
+            <ButtonSecondary name='Wishlist Item'  onClick={()=>{alert('Hello')}}/>
+          </div>
+          <h6 class="mt-2">{craft[0].description}</h6>
+          <p>Max quantity: {craft[0].quantity}</p>
         </div>
-        <h6 class="mt-2">{craft[0].description}</h6>
-        <div class="d-flex gap-4 p-2">
-          <h6>Select Quantity:</h6>
-          <h4>{quantity}</h4>
-          <Fab size="small"  aria-label="add" onClick={()=>{quantityHandler(quantity+1)}}>
-            <AddIcon />
-          </Fab>
-          <Fab size="small" aria-label="remove" onClick={()=>{quantityHandler(quantity-1)}}>
-            <RemoveIcon/>
-          </Fab>
+        <div style={{marginLeft:'15vw',display:'flex',flexDirection:'column', boxShadow:'0px 0px 10px 0px()',padding:'10px'}}>
+          <div><h3>Crafter Details:</h3></div>
+          <div class="d-flex gap-3">
+            <div>
+              <div class="d-flex">Crafter Name:<h5><b>{crafter.name}</b></h5></div>
+              <div class="d-flex">House Number:<h5><b>{crafter.houseNumber}</b></h5></div>
+              <div class="d-flex">Latitude:<h5><b>{crafter.lattitude}</b></h5></div>
+              <div class="d-flex">Longitude:<h5><b>{crafter.longitude}</b></h5></div>
+            </div>
+            <div><img style={{width:'20vw', height:'20vh',maxHeight:'125px',maxWidth:'125px',objectFit:'cover',borderRadius:'50%',padding:'1px'}} src={crafter.profilePicUrl} alt="" class="w-10 h-10"/></div>
+          </div>
         </div>
-        <p>Max quantity: {craft[0].quantity}</p>
       </div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Logistics"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            <div class="d-flex gap-4 p-2">
+              <h6>Select Quantity:</h6>
+              <h4>{quantity}</h4>
+              {/* <Fab size="small"  aria-label="add" > */}
+                <AddIcon type="submit" onClick={()=>{quantityHandler(quantity+1)}}/>
+              {/* </Fab> */}
+              {/* <Fab size="small" aria-label="remove" onClick={()=>{quantityHandler(quantity-1)}}> */}
+                <RemoveIcon type="button" onClick={()=>{quantityHandler(quantity-1)}}/>
+              {/* </Fab> */}<br/>
+              <h5>Payable Amount:{price * quantity}</h5>
+            </div>
+            <TextField fullWidth id="outlined-basic" label="Enter customization details" variant="outlined" value={userMessage} onChange={(e)=>{setUserMessage(e.target.value)}}/>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button autoFocus onClick={orderRequestHandler}>
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>:
 
     <>
       Item Not Found;
     </>
-
 
   )
 }
