b87b67ebd1a4d521be80e86c0b1b93555ab46c04
Shaik Abid Hussain <sabidhussain@BLR.ALLEGISINDIA.COM>
2024-10-14 12:07:13 +0530

    fixing profile page bug post optimization

--- homie-craft/src/pages/crafter/CrafterProfile.jsx
+++ homie-craft/src/pages/crafter/CrafterProfile.jsx
@@ -43,8 +43,6 @@ function CrafterProfile() {
       city : userData.city,
       state : userData.state,
       pinCode : userData.pinCode,
-      latitude: userData.latitude,
-      longitude: userData.longitude,
       PickUpFromLocation: true
     },
     enableReinitialize:true,
@@ -55,9 +53,7 @@ function CrafterProfile() {
       city: Yup.string().required("City is Required"),
       state: Yup.string().required("State is Required"),
       pinCode: Yup.number().required("Pincode is required").min(100000,"Enter a valid Pincode").max(999999,"Enter a valid Pincode"),
-      latitude: Yup.number().required('Latitude is required').min(-90, 'Latitude must be between -90 and 90').max(90, 'Latitude must be between -90 and 90'),
-  longitude: Yup.number().required('Longitude is required').min(-180, 'Longitude must be between -180 and 180').max(180, 'Longitude must be between -180 and 180')
-    }),
+     }),
     onSubmit: (values)=>{
       const formData = new FormData();
       for (let i = 0; i < image.length; i++) {
@@ -65,14 +61,11 @@ function CrafterProfile() {
       }
       formData.append('Name',values.name);
       formData.append('Mobile',values.mobile);
-      formData.append('Password',"********");
-      formData.append('HouseNumber',values.houseNumber);
+     formData.append('HouseNumber',values.houseNumber);
       formData.append('City',values.city);
       formData.append('State',values.state);
       formData.append('PinCode',values.pinCode);
-      formData.append('Longitude',values.longitude);
-      formData.append('Latitude',values.latitude);
-      try {
+       try {
         axios.put(baseURL+"crafter/"+userId,formData,{
           headers: {
             
@@ -218,36 +211,7 @@ function CrafterProfile() {
         
         </Container>
 
-        <Container item xs={12} sx={{padding:"5px"}}>
-          
-      <label htmlFor="latitude">Latitude</label>
-          <TextField
-            fullWidth
-            id="latitude"
-            name="latitude"
-            type="number"
-            value={formik.values.latitude}
-            onChange={formik.handleChange}
-            error={formik.touched.latitude && Boolean(formik.errors.latitude)}
-            helperText={formik.touched.latitude && formik.errors.latitude}
-          />
         
-        </Container>
-        <Container item xs={12} sx={{padding:"5px"}}>
-          
-      <label htmlFor="longitude">Longitude</label>
-          <TextField
-            fullWidth
-            id="longitude"
-            name="longitude"
-            type="number"
-            value={formik.values.longitude}
-            onChange={formik.handleChange}
-            error={formik.touched.longitude && Boolean(formik.errors.longitude)}
-            helperText={formik.touched.longitude && formik.errors.longitude}
-          />
-        
-        </Container>
         
       </Container>
       <Container>
