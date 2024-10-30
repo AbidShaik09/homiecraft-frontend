8953dce50aa48dffca56ccf3836f6fffdd6681a8
Anavena Ajay <aajay@BLR.ALLEGISINDIA.COM>
2024-10-30 14:35:50 +0530

    Fixed order page

--- homie-craft/src/pages/customer/Orders.jsx
+++ homie-craft/src/pages/customer/Orders.jsx
@@ -1,4 +1,4 @@
-import { Container, Skeleton, Stack, Typography } from '@mui/material'
+import { Container, Skeleton, Snackbar, Stack, Typography } from '@mui/material'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import OrderDetails from '../../components/orderdetails/OrderDetails'
@@ -8,33 +8,25 @@ import { Fullscreen } from '@mui/icons-material'
 function Orders() {
     const baseURL = "http://localhost:5265/"
     const [orders, setOrders] = useState([])
+    const [loading,setLoading] = useState(false);
     let id = localStorage.getItem("id")
     useEffect(() => {
+        setLoading(true);
         axios.get(baseURL + "order/customer/" + id).then(e => {
             var o = e.data.sort((a, b) => b.isActive - a.isActive);
             setOrders(o);
             console.log(orders);
-
-        }
-        )
+            setLoading(false);
+            }
+        ).catch(e =>{
+            setLoading(false);
+            <Snackbar title='Error occured'/>
+        })
 
     }, [])
-    
-    return (
-        <Container sx={{marginTop:"20px"}}>
-            <Container  >
-                <Typography variant='h4' sx={{marginLeft:"20px"}}>
-                Orders
-                </Typography>
-            </Container>
-            <Container >
 
-                {
-                    orders.length > 0 ?
-                        orders.map(order => {
-                            return <CustomerOrder order={order }/>
-                        }) : 
-                        <Container>
+    if(loading){
+        <Container>
                         <Skeleton
                         sx={{ bgcolor: 'grey.210', borderRadius:"20px", marginBottom:"20px"}}
                         variant="rectangular"
@@ -48,6 +40,23 @@ function Orders() {
                         height={300}
                       />
                       </Container>
+    }
+    
+    return (
+        <Container sx={{marginTop:"20px"}}>
+            <Container  >
+                <Typography  sx={{marginBottom:"20px", fontSize:"30px"}}>
+                Orders
+                </Typography>
+            </Container>
+            <Container >
+
+                {
+                    orders.length > 0 ?
+                        orders.map(order => {
+                            return <CustomerOrder order={order }/>
+                        }) : 
+                        <div>No orders.</div>
                 }
 
             </Container>
